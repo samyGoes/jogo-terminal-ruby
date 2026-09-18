767fb851c8ef969bb1c26c9ddb522ea9fb743074
Sâmily <goessamily@gmail.com>
2026-09-18 19:45:00 -0300

    feat: Opção para mostrar o texto de uma vez se apertar enter ou espaço

--- lib/modules/Efeitos.rb
+++ lib/modules/Efeitos.rb
@@ -1,18 +1,38 @@
+require "io/console"
+
 module Efeitos
 
+  # Printa um texto caractere por caractere (como se estivesse digitando).
+  # 
+  # @param texto [String] O texto a ser escrito
+  # @param velocidade [Number] Tempo que demorará para escrever o próximo caractere
+  # @return [void]
   def maquina_texto(texto, velocidade = 0.05)
     @txt_l = texto.length
+    @caractere = nil
 
+    # Ajuste da velocidade que o texto aparece de acordo com a qtd de caracteres
     if @txt_l < 6 then velocidade = 0.09
     elsif @txt_l > 20 then velocidade = 0.02
     end
 
-    texto.each_char do |char|
-      print char
-      #$stdout.flush
-      sleep velocidade
+    # Printando o texto
+    texto.each_char.with_index do |char, i|
+      
+      # Quando já tiver três caracteres ou mais printados e clicar em alguma tecla
+      if i >= 3 and IO.select([$stdin], nil, nil, 0)
+        @caractere = STDIN.getch #Pega qual tecla foi
+      end
+      
+      # Se a tecla clicada for enter ou espaço printa o restante do texto inteiro
+      if @caractere == "\r" or @caractere == " "
+        print @restante_txt = texto[i, @txt_l]
+        break
+      end
+      print char # Printa o caractere atual 
+      sleep velocidade # Espera certo tempo até o programa continuar
     end
-    puts #pula linha pro próximo texto
+    puts # Pula linha pro próximo texto
   end
 
 end
